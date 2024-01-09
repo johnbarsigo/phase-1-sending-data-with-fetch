eb4eb6657e56742be01faf47dd449d700f980f0e
johnbarsigo <john.barsigo@student.moringaschool.com>
2024-01-09 09:39:51 +0300

    In Progress

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+const userData = {
+    name : "Ken-Block",
+    email : "ken43@hoonigan.com"
+}
+
+const configurationObject = {
+    method : "POST",
+    headers : {
+        "Content-Type" : "application/json",
+        Accept : "application/json",
+    },
+    body : JSON.stringify (userData),
+};
+
+fetch("http://localhost:3000/users", configurationObject);
